493327f0bdbc8bf61283a816ea043b5e96f4123e
m3adow <mail-git@till-wiese.de>
2016-07-13 20:40:03 +0200

    Found a better way for injecting.

--- ceph/ceph_envs.sh
+++
@@ -1,7 +0,0 @@
-MON_IP=$(giddyup ip myip)
-CEPH_PUBLIC_NETWORK=$(ipcalc $(ip a | grep "$(giddyup ip myip)" \
-  | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}') | grep -E '^Network:' \
-  | awk '{print $2}')
-
-export MON_IP
-export CEPH_PUBLIC_NETWORK
