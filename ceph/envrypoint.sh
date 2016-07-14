4ff02bf8f63a53148a04ca71d7029effde26fb03
Till Wiese <mail-github@till-wiese.de>
2016-07-14 14:36:19 +0200

    Pinpointing before the error is better...

--- ceph/envrypoint.sh
+++ ceph/envrypoint.sh
@@ -6,9 +6,6 @@ set -o pipefail
 if [ -n "${RANCHER}" ]
 then
   MON_IP=$(giddyup ip myip 2>/dev/null)
-  CEPH_PUBLIC_NETWORK=$(ipcalc $(ip a | grep "$(giddyup ip myip)" \
-    | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}') | grep -E '^Network:' \
-    | awk '{print $2}')
   # Trying to find a randomly happening error (about 2/3 of the tries) somewhere in the CEPH_PUBLIC_NETWORK calculation
   A=$(ip a | grep "$(giddyup ip myip)")
   B=$(echo "$A"| grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}')
@@ -16,6 +13,10 @@ then
   D=$(echo "$C" | grep -E '^Network:')
   E=$(echo "$D" | awk '{print $2}')
   echo "$E"
+  CEPH_PUBLIC_NETWORK=$(ipcalc $(ip a | grep "$(giddyup ip myip)" \
+    | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}') | grep -E '^Network:' \
+    | awk '{print $2}')
+  
   export CEPH_PUBLIC_NETWORK
   export MON_IP
 fi
