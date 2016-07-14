9f691e72ecd728f3a06e9131224e0d03334fdbd9
m3adow <mail-github@till-wiese.de>
2016-07-14 14:17:10 +0200

    Trying to pinpoint a strange error

--- ceph/envrypoint.sh
+++ ceph/envrypoint.sh
@@ -9,6 +9,13 @@ then
   CEPH_PUBLIC_NETWORK=$(ipcalc $(ip a | grep "$(giddyup ip myip)" \
     | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}') | grep -E '^Network:' \
     | awk '{print $2}')
+  # Trying to find a randomly happening error (about 2/3 of the tries) somewhere in the CEPH_PUBLIC_NETWORK calculation
+  A=$(ip a | grep "$(giddyup ip myip)")
+  B=$(echo "$A"| grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}')
+  C=$(ipcalc $B)
+  D=$(echo "$C" | grep -E '^Network:')
+  E=$(echo "$D" | awk '{print $2}')
+  echo "$E"
   export CEPH_PUBLIC_NETWORK
   export MON_IP
 fi
