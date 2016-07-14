870d55bb395a29e315f4c9a01441420d58484f6e
m3adow <mail-git@till-wiese.de>
2016-07-14 17:10:19 +0200

    Trying to avoid race condition with network

--- ceph/envrypoint.sh
+++ ceph/envrypoint.sh
@@ -5,22 +5,20 @@ set -o pipefail
 
 if [ -n "${RANCHER}" ]
 then
-  MON_IP=$(giddyup ip myip 2>/dev/null)
-  # Trying to find a randomly happening error (about 2/3 of the tries) somewhere in the CEPH_PUBLIC_NETWORK calculation
-  giddyup ip myip
-  ip a
-  ip a | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}'
-  
-  A=$(ip a | grep "$(giddyup ip myip)")
-  B=$(echo "$A"| grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}')
-  C=$(ipcalc $B)
-  D=$(echo "$C" | grep -E '^Network:')
-  E=$(echo "$D" | awk '{print $2}')
-  echo "$E"
-  CEPH_PUBLIC_NETWORK=$(ipcalc $(ip a | grep "$(giddyup ip myip)" \
-    | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}') | grep -E '^Network:' \
-    | awk '{print $2}')
-  
+  MON_IP=${MON_IP:-}
+  # Wait until giddyup returns an IP
+  while [ -z "${MON_IP}" ]
+  do
+    MON_IP=$(giddyup ip myip 2>/dev/null)
+  done
+
+  # Then wait for network readiness
+  while [ -z "$(ip a | grep "${MON_IP}")" ]
+  do
+    CEPH_PUBLIC_NETWORK=$(ipcalc $(ip a | grep "${MON_IP}" \
+      | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}') | grep -E '^Network:' \
+      | awk '{print $2}')
+  done 
   export CEPH_PUBLIC_NETWORK
   export MON_IP
 fi
