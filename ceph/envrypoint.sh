2a892a3a94e0e8911e78df2e6248824de2c8a4a7
m3adow <mail-github@till-wiese.de>
2016-07-14 14:00:53 +0200

    Carryover.

--- ceph/envrypoint.sh
+++ ceph/envrypoint.sh
@@ -5,16 +5,12 @@ set -o pipefail
 
 if [ -n "${RANCHER}" ]
 then
-  MON_IP=$(giddyup ip myip 2>/dev/null|| printf '')
-
-  if [ -n "${MON_IP}" ]
-  then
-    CEPH_PUBLIC_NETWORK=$(ipcalc $(ip a | grep "$(giddyup ip myip)" \
-      | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}') | grep -E '^Network:' \
-      | awk '{print $2}' || printf '')
-    export CEPH_PUBLIC_NETWORK
-    export MON_IP
-  fi
+  MON_IP=$(giddyup ip myip 2>/dev/null)
+  CEPH_PUBLIC_NETWORK=$(ipcalc $(ip a | grep "$(giddyup ip myip)" \
+    | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}') | grep -E '^Network:' \
+    | awk '{print $2}')
+  export CEPH_PUBLIC_NETWORK
+  export MON_IP
 fi
 
 exec /entrypoint.sh "$@"
